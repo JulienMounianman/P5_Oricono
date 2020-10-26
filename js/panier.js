82cbf55292f7e129399cda2414624cf4b5810119
JulienMounianman <julienmounianman@gmail.com>
2020-10-26 12:24:12 +0100

    asynchrone

--- js/panier.js
+++ js/panier.js
@@ -6,99 +6,160 @@ var panier = localStorage;
 var size = Object.keys(panier).length;
 var tabPanier = [];
 var clear = document.getElementById("clear");
+var urlApi = "http://localhost:3000/api/teddies";
 tabPanier.length = size;
 
-//Verification parametre URl
-if(params.has('id')) {
-  var id = params.get('id');
-  if (size == 0) {
-    panier.setItem('Ours', id);
-  } else {
-    var nb = size;
-    panier.setItem('Ours'+ nb,id)
-  }
-  params.delete('id');
-}
 
-//Ajout des articles du panier dans un Tableau js
-for (var i= 0; i <= size; i++) {
-  if (i == 0) {
-    tabPanier[i] = panier.getItem('Ours');
-  }else{
-    if(panier.getItem('Ours'+ i) != null) {
-      tabPanier[i] = panier.getItem('Ours'+ i);
+function GestionPanier() {
+  //Verification parametre URl
+  if(params.has('id')) {
+    var id = params.get('id');
+    if (size == 0) {
+      panier.setItem('Ours', id);
+    } else {
+      var nb = size;
+      panier.setItem('Ours'+ nb,id)
     }
+    params.delete('id');
   }
-}
-
-//Bouton vider le panier
-clear.onclick = function() {
-  panier.clear();
-}
-
-//Requetes get pour afficher article panier
-request.onreadystatechange = function() {
-    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-      var json = JSON.parse(request.responseText);
-      var results = json;
-      var teddies = [];
-      var prixtotal = 0;
-      teddies.length = size;
-      var test = '';
-      for (var x in results) {
-        for (var i= 0; i <= tabPanier.length; i++) {
-          if (results[x]._id == tabPanier[i]) {
-            var color = "secondary";
-            switch (results[x]._id) {
-              case '5be9c8541c9d440000665243':
-              color = "secondary";
-              break;
-              case '5beaa8bf1c9d440000a57d94':
-              color = "danger";
-              break;
-              case '5beaaa8f1c9d440000a57d95':
-              color = "info";
-              break;
-              case '5beaabe91c9d440000a57d96':
-              color = "success";
-              break;
-              case '5beaacd41c9d440000a57d97':
-              color = "dark";
-            }
-          var test =
-          '<div class="col-sm-3">'+
-            '<div class="card text-white bg-'+color+' mb-3" style="max-width: 18rem;">'+
-              '<div class="card-header">'+ results[x].name  +'</div>'+
-              '<div class="card-body">'+
-                '<h5 class="card-title">'+ results[x].price  +'</h5>'+
-                '<p class="card-text"></p>'+
-              '</div>'+
-            '</div>'+
-          '</div>';
-          teddies[i] = test
-
-          prixtotal = prixtotal + results[x].price;
-          }
+  //Ajout des articles du panier dans un Tableau js
+    for (var i= 0; i <= size; i++) {
+      if (i == 0) {
+        tabPanier[i] = panier.getItem('Ours');
+      }else{
+        if(panier.getItem('Ours'+ i) != null) {
+          tabPanier[i] = panier.getItem('Ours'+ i);
         }
       }
-    allArticlePanier = teddies.join("");
-    var afficherPrixTotal = "Prix total : " + prixtotal;
-    document.getElementById("ListeArcticlePanier").innerHTML = allArticlePanier;
-    document.getElementById("prixtotal").innerHTML = afficherPrixTotal;
-    } else {
-      var test = '<div class="col-md">' +
-                    '<div class="alert alert-primary text-center" role="alert">'+
-                    'Impossible de se connecter à l'+"'"+'API.'+
-                    '</div>'+
-                  '</div>';
-      document.getElementById("ListeArcticlePanier").innerHTML = test;
+    }
+}
+
+
+
+
+
+function affichagePanier (allteddies) {
+  var json = JSON.parse(allteddies);
+  var results = json;
+  var teddies = [];
+  var prixtotal = 0;
+  teddies.length = size;
+  var test = '';
+
+  for (var x in results) {
+    for (var i= 0; i <= tabPanier.length; i++) {
+      if (results[x]._id == tabPanier[i]) {
+        var color = "secondary";
+        switch (results[x]._id) {
+          case '5be9c8541c9d440000665243':
+          color = "secondary";
+          break;
+          case '5beaa8bf1c9d440000a57d94':
+          color = "danger";
+          break;
+          case '5beaaa8f1c9d440000a57d95':
+          color = "info";
+          break;
+          case '5beaabe91c9d440000a57d96':
+          color = "success";
+          break;
+          case '5beaacd41c9d440000a57d97':
+          color = "dark";
+        }
+      var test =
+      '<div class="col-sm-3">'+
+        '<div class="card text-white bg-'+color+' mb-3" style="max-width: 18rem;">'+
+          '<div class="card-header">'+ results[x].name  +'</div>'+
+          '<div class="card-body">'+
+            '<h5 class="card-title">'+ results[x].price  +'</h5>'+
+            '<p class="card-text"></p>'+
+          '</div>'+
+        '</div>'+
+      '</div>';
+      teddies[i] = test
+
+      prixtotal = prixtotal + results[x].price;
+      }
     }
   }
-  request.open("GET", "http://localhost:3000/api/teddies");
-  request.send();
+  allArticlePanier = teddies.join("");
+  var afficherPrixTotal = "Prix total : " + prixtotal;
+  document.getElementById("ListeArcticlePanier").innerHTML = allArticlePanier;
+  document.getElementById("prixtotal").innerHTML = afficherPrixTotal;
+}
+
+
+
+function getallteddies(url) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+    xhr.onload = function() {
+      if (xhr.status != 200) { // analyze HTTP status of the response
+        reject('Error'+xhr.status+':'+ xhr.statusText); // e.g. 404: Not Found
+      } else { // show the result
+        resolve(xhr.responseText); // response is the server
+      }
+    }
+    xhr.onerror = function() {
+      reject("Request failed");
+    };
+    xhr.send();
+  });
+}
+
+function redirectForm(response) {
+  var json = JSON.parse(response);
+  var results = json;
+  panier.clear();
+  var price = 0;
+  for(i=0;i<results.products.length;i++){
+    price = price + results.products[i].price;
+  }
+  var confirmUrl = url.pathname.replace('panier','confirmation') +
+                '?id=' + results.orderId +
+                '&price=' + price;
+  window.location.href = confirmUrl;
+}
+
+
+function postForm(url,json) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", url + '/order');
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onload = function() {
+      if (xhr.status != 201) { // analyze HTTP status of the response
+        reject('Error'+xhr.status+':'+ xhr.statusText); // e.g. 404: Not Found
+      } else { // show the result
+        resolve(xhr.responseText); // response is the server
+      }
+    }
+    xhr.onerror = function() {
+      reject("Request failed");
+    };
+    xhr.send(json);
+  });
+}
+
+
+
+
+var teddies = getallteddies(urlApi);
+    teddies.then((value) => {
+      GestionPanier();
+      affichagePanier(value);
+
+    }).catch((error) => {
+      console.log(error);
+    })
+
+//Bouton vider le panier
+  clear.onclick = function() {
+    panier.clear();
+  }
 
 //test Formulaire
-
 function ValidateEmail(email)
 {
   var mailformat =  RegExp(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/);
@@ -108,7 +169,6 @@ function ValidateEmail(email)
     return false;
   }
 }
-
 function validateInput(input,nbCharactere) {
   var  regex = RegExp('^[a-zA-Z]+$');
   if(regex.test(input)){
@@ -121,8 +181,16 @@ function validateInput(input,nbCharactere) {
     return false;
   }
 }
-
-
+function validatePanier(panier) {
+  return new Promise((resolve, reject) => {
+    if(panier[0] != null) {
+      resolve("Le Panier n'est pas vide");
+    }else {
+      reject("Le Panier est vide")
+    }
+  });
+}
+//Formulaire event
 (function() {
   window.addEventListener("load", function() {
     var form = document.getElementById("form")
@@ -154,28 +222,23 @@ function validateInput(input,nbCharactere) {
 
         var testjson = JSON.stringify(finalObj);
         console.log(testjson);
-        request.open("POST", "http://localhost:3000/api/teddies/order");
-        request.setRequestHeader("Content-Type", "application/json");
-        request.send(testjson);
-        if(tabPanier[0] != null) {
-          request.onreadystatechange = function() {
-            if (this.readyState == XMLHttpRequest.DONE && this.status == 201) {
-              var json = JSON.parse(request.responseText);
-              var results = json;
-              panier.clear();
-              var price = 0;
-              for(i=0;i<results.products.length;i++){
-                price = price + results.products[i].price;
-              }
-              var confirmUrl = url.pathname.replace('panier','confirmation') +
-                            '?id=' + results.orderId +
-                            '&price=' + price;
-              window.location.href = confirmUrl;
-            }
-          }
-        }else {
-          window.alert("Panier vide !");
-        }
+
+        var verifPanier = validatePanier(tabPanier);
+        verifPanier.then((value) => {
+          var result = postForm(urlApi,testjson);
+          result.then((value) => {
+              redirectForm(value);
+          }).catch((error) => {
+            console.log(error);
+          })
+        }).catch((error) => {
+          var panier_vide = '<div class="col-md-12">' +
+                        '<div class="alert alert-danger text-center" role="alert">'+
+                          error +
+                        '</div>'+
+                      '</div>';
+          document.getElementById("ListeArcticlePanier").innerHTML = panier_vide;
+        })
       form.classList.add("was-validated")
     }, false)
   }, false)
